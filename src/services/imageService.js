ea4667061b56c22cc639859c308d0326388f8a0f
Melih Çelik <melihcelik0909@gmail.com>
2022-10-23 21:33:04 +0300

    singleUpload Completed

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -1,5 +1,12 @@
 class ImageService {
-  singleImageUpload = async (imagePath, file) => {};
+  singleImageUpload = async (imagePath, image) => {
+    image.mv(imagePath, (err) => {
+      if (err) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
+      }
+    });
+    return image;
+  };
   multipleImageUpload = async (imagePath, images) => {
     if (!Array.isArray(images)) {
       console.log("You must select more than one image");
