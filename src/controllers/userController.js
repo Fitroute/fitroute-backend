ee2def3a87e82aed892916d7da67a73ba586c5dd
Melih Çelik <melihcelik0909@gmail.com>
2023-03-09 17:51:20 +0300

    quickfix:number to string

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -133,7 +133,7 @@ const sendCode = async (req, res) => {
       return;
     }
     // const code = uuid.v4()?.split("-")[0] || `fit-${new Date().getTime()}`;
-    const code = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+    const code = `${Math.floor(Math.random() * (9999 - 1000)) + 1000}`;
     updateWithEmail({ email: req.body.email }, { resetCode: code }).then(
       (response) => {
         sendMail({
